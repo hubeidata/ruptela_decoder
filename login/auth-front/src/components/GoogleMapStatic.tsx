6835d98b13dc155e5380fc78244963dcf572d437
Vidal Alvarez Manuel <valvarezma@unsa.edu.pe>
2025-05-19 15:50:33 -0500

    actualizando

--- login/auth-front/src/components/GoogleMapStatic.tsx
+++ login/auth-front/src/components/GoogleMapStatic.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { APIProvider, Map, AdvancedMarker, Pin } from "@vis.gl/react-google-maps";
 
 const containerStyle = {
-  position: "absolute", // Ocupa toda la pantalla
-  top: 0,
-  left: 0,
+  position: "relative", // Cambiado a relativo para respetar el flujo del diseño
   width: "100%",
-  height: "100%",
+  height: "calc(100vh - 60px)", // Ajusta la altura para dejar espacio al menú superior
+  marginTop: "60px", // Asegura que el mapa no se superponga al menú
+  pointerEvents: "auto", // Permite que los eventos del mouse lleguen al mapa
 };
 
 const center = { lat: -16.410471, lng: -71.53088 };
@@ -30,11 +30,12 @@ export default function GoogleMapStatic() {
           heading={90}
           style={{ width: "100%", height: "100%" }}
           options={{
-            zoomControl: true, // Habilita el control de zoom
+            zoomControl: true, // Habilita los botones de zoom
             scrollwheel: true, // Permite hacer zoom con la rueda del ratón
             draggable: true, // Permite arrastrar el mapa
             fullscreenControl: true, // Habilita el control de pantalla completa
             mapTypeControl: true, // Habilita el control de tipo de mapa
+            gestureHandling: "auto", // Permite gestos como scroll y arrastre
           }}
         >
           {points.map((point, idx) => (
