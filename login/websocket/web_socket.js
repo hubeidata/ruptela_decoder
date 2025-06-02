9ba74568aa4acd8bd89fc3d2e0e2e8dd42bccc89
Vidal Alvarez Manuel <valvarezma@unsa.edu.pe>
2025-06-02 10:52:38 -0500

    actualizar

--- login/websocket/web_socket.js
+++ login/websocket/web_socket.js
@@ -1,7 +1,7 @@
 import WebSocket from 'ws';
 import { encrypt } from './utils/encrypt.js';  // o donde esté tu función
 
-const WS_URL = 'ws://ruptela.santiago.maxtelperu.com';  // o la URL pública
+const WS_URL = 'wss://ruptela.santiago.maxtelperu.com';  // o la URL pública
 //const WS_URL = 'ws://localhost:5000';  // o la URL pública
 
 // 1) Conecta
