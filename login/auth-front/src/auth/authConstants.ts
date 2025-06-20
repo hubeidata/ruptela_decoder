79866e94fd332ecde3b9adba5c113f44841b2622
Vidal Alvarez Manuel <valvarezma@unsa.edu.pe>
2025-06-20 11:58:16 -0500

    volviendo a https

--- login/auth-front/src/auth/authConstants.ts
+++ login/auth-front/src/auth/authConstants.ts
@@ -1,6 +1,6 @@
 const isProduction = window.location.hostname !== "localhost";
 
 export const API_URL = isProduction
-  ? "http://api.santiago.maxtelperu.com"
+  ? "https://api.santiago.maxtelperu.com"
   : "http://localhost:3000";
 
