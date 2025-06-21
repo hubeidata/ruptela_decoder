e3955f7bff697f5a21b412f5cafcdab635b5e913
Vidal Alvarez Manuel <valvarezma@unsa.edu.pe>
2025-06-21 17:43:46 -0500

    cambiando en cono se conecta a la base de datos CommonJS

--- login/auth-back/config/database.js
+++ login/auth-back/config/database.js
@@ -1,5 +1,5 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
@@ -21,4 +21,4 @@ const sequelize = new Sequelize(
   }
 );
 
-export default sequelize;
+module.exports = sequelize;
